2d248c05dab8a88dd62ec9ce15ff78a5db0e09fd
lukemolloy <luke_d_a@hotmail.com>
2016-09-27 18:21:22 +1000

    Luke

--- server.c
+++ server.c
@@ -10,8 +10,11 @@
 #include <unistd.h>
 #include <errno.h>
 
-const char delimiters[] = "	 \n";
+const char delimiters[] = "	 \n,";
 char *Authentication[11][3];
+char *Accounts[25][3];
+char *ClientDetails[][6];
+char *Transactions[1][4];
 
 void tokenAuth(){
 	FILE  *file;
@@ -40,16 +43,80 @@ void tokenAuth(){
     fclose(file);
 }
 
+void tokenAccount(){
+    FILE  *file;
+    char *string, *found;
+
+    file = fopen("Accounts.txt", "r");
+    
+    char line[256];
+    int x = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+
+        string = strdup(line);
+
+        found = strtok (string, delimiters);
+        Accounts[x][0]=found;
+
+        found = strtok (NULL, delimiters);
+        Accounts[x][1]=found;
+
+        found = strtok (NULL, delimiters);
+        Accounts[x][2]=found;
+        
+        x++;
+    }
+    fclose(file);
+}
+
+void tokenClient(){
+    FILE  *file;
+    char *string, *found;
+
+    file = fopen("Client_Details.txt", "r");
+    
+    char line[256];
+    int x = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+
+        string = strdup(line);
+
+        found = strtok (string, delimiters);
+        ClientDetails[x][0]=found;
+
+        found = strtok (NULL, delimiters);
+        ClientDetails[x][1]=found;
+
+        found = strtok (NULL, delimiters);
+        ClientDetails[x][2]=found;
+
+        found = strtok (NULL, delimiters);
+        ClientDetails[x][3]=found;
+
+        found = strtok (NULL, delimiters);
+        if(found == NULL){
+            ClientDetails[x][4] = " ";
+        }else{
+            ClientDetails[x][4]=found;
+        }
+
+        found = strtok (NULL, delimiters);
+        if(found == NULL){
+            ClientDetails[x][5] = " ";
+        }else{
+            ClientDetails[x][5]=found;
+        }
+        
+        x++;
+    }
+    fclose(file);  
+}
 
 int main(int argc, char *argv[]) {
 
-	tokenAuth();
 
-    /*for(int x = 0; x<11; x++){
-    	printf("%s\n", "");
-    	for(int y = 0; y<3; y++){
-    		printf("%s\n", Authentication[x][y]);
-    	}
-    }*/
 
+    return 0;
 }
